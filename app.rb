bccd8cabbc66fe13f284313bd8582e3dc7599cf3
cassidytrinh <cassidytrinh@gmail.com>
2018-06-26 15:56:02 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/:name' do
+    @user_name = params[:name]
+    "#{@user_name.capitalize} is the best!"
+  end
 
+  get '/reversename/:name' do
+    @name = params[:name]
+    @reversed = @name.reverse
+    @reversed
+  end
+
+  get '/say/:number/:phrase' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    return @phrase * @num
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+  @words = params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]
+  @words
 end
